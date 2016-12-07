d581a70ffeb9de99b0da7e2a3ecbd44155d47b84
Rocio Mercado <rocio@berkeley.edu>
2016-12-06 21:25:03 -0800

    Fixed error, now have 2 lines per cycle instead of just one with everything on it

--- workingScripts/create-InputMSD.sh
+++ workingScripts/create-InputMSD.sh
@@ -4,8 +4,10 @@ echo -n "# this line is a comment" >> traj.GUEST.10.dump.dat
 echo -n "# this line is a comment" >> traj.GUEST.10.dump.dat
 cat traj.GUEST.10.dump >> traj.GUEST.10.dump.dat
 numberOfAtoms=11
-numberOfAtomsPlus=$(echo "$numberOfAtoms + 1" | bc -l)
+numberOfAtomsPlus=$(echo "$numberOfAtoms + 2" | bc -l)
+sed -i '/ITEM\: BOX BOUNDS/i newline' traj.GUEST.10.dump.dat
 sed -i '/ITEM\: BOX BOUNDS/,+3d' traj.GUEST.10.dump.dat
 sed -i '/ITEM/d' traj.GUEST.10.dump.dat
 a="\$!N;"; b=""; for i in $(seq 1 $numberOfAtomsPlus); do b+=$a; done
 sed -i ''"$b"'s/\n/ /g' traj.GUEST.10.dump.dat
+sed -i 's/newline /\n/g' traj.GUEST.10.dump.dat
