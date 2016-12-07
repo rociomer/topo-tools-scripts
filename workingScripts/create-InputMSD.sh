c8f7e1525ba61dc9843cdc97a2c1e8ef006eb90a
Rocio Mercado <rocio@berkeley.edu>
2016-12-06 20:58:34 -0800

    Updated create-InputMSD.sh

--- workingScripts/create-InputMSD.sh
+++ workingScripts/create-InputMSD.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-cp traj.GUEST.10.dump.test.copy traj.GUEST.10.dump.test
+echo -n "# this line is a comment" > traj.GUEST.10.dump.dat
+echo -n "# this line is a comment" >> traj.GUEST.10.dump.dat
+echo -n "# this line is a comment" >> traj.GUEST.10.dump.dat
+cat traj.GUEST.10.dump >> traj.GUEST.10.dump.dat
 numberOfAtoms=11
 numberOfAtomsPlus=$(echo "$numberOfAtoms + 1" | bc -l)
-sed -i '/ITEM\: BOX BOUNDS/,+3d' traj.GUEST.10.dump.test
-sed -i '/ITEM/d' traj.GUEST.10.dump.test
+sed -i '/ITEM\: BOX BOUNDS/,+3d' traj.GUEST.10.dump.dat
+sed -i '/ITEM/d' traj.GUEST.10.dump.dat
 a="\$!N;"; b=""; for i in $(seq 1 $numberOfAtomsPlus); do b+=$a; done
-sed -i ''"$b"'s/\n/ /g' traj.GUEST.10.dump.test
+sed -i ''"$b"'s/\n/ /g' traj.GUEST.10.dump.dat
