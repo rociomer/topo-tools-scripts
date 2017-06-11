e5f3039c7080fe58dd767738af8a71a1b6457ecf
Rocio Mercado <rocio@berkeley.edu>
2017-06-11 11:05:06 -0700

    Added toposcripts for the extended framework, all metals, both charged and uncharged, as well as modified create*sh for CO2 in the extended framework

---
+++ workingScripts/allocate-files-CO2.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+for i in Mg Ni Zn; do mkdir ${i}-MOF-74-CO2-298K; for j in 1000 2500 5000 7500 10000 25000 50000 75000 100000 250000 500000 750000 1000000 2500000 5000000 7500000 10000000; do mkdir ${i}-MOF-74-CO2-298K/Pressure${j}; mv ${i}*Pressure${j}.* ${i}-MOF-74-CO2-298K/Pressure${j}/.; done; done
---
+++ workingScripts/test.sh
@@ -0,0 +1,6 @@
+temp="298.0"
+tempInt=${temp%.*}
+
+echo $temp
+echo $tempInt
+
