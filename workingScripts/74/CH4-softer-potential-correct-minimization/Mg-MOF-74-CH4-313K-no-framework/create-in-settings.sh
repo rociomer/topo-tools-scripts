9a01c80c228cf6b52f8b658240c106a92b118fc2
Rocio Mercado <rocio@berkeley.edu>
2018-03-07 05:07:47 -0800

    Reorganized

--- workingScripts/74/CH4-softer-potential-correct-minimization/Mg-MOF-74-CH4-313K-no-framework/create-in-settings.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-for i in 10000 50000 100000 200000 300000 400000 500000 1000000 2000000 3000000 4000000 5000000 10000000
-do
-  cd Pressure${i}/
-  cp ../Pressure10000/MgMOF74-Pressure10000.in.settings MgMOF74-Pressure${i}.in.settings
-  cd ../
-done
--- workingScripts/74/CO2-correct-minimization/modify-fix.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-for i in */*/*.in
-do 
-  sed -i '69d' ${i}
-  sed -i '69d' ${i}
-  sed -i '70ifix_modify         3 temp allTemp' ${i}
-  sed -i '70ifix                3 GUEST rigid/nvt molecule temp 298.0 298.0 100.0' ${i}
-done
